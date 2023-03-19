b0cc3fac643f25223fe1466cd49970be602df241
parth-nikam <parthsnikam12co@gmail.com>
2023-03-19 12:54:29 +0530

    Using UIkit component in SwiftUI. Completed the App.

---
+++ Phisher News/Views/DetailView.swift
@@ -0,0 +1,28 @@
+//
+//  DetailView.swift
+//  Phisher News
+//
+//  Created by Parth Nikam on 19/03/23.
+//
+
+import SwiftUI
+
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+
+
+
