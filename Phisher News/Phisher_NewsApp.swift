dc6bc87e4df04ac8ca714193d21965e5a6f3fac1
parth-nikam <parthsnikam12co@gmail.com>
2023-03-19 09:52:00 +0530

    Initial Commit

---
+++ Phisher News/Phisher_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phisher_NewsApp.swift
+//  Phisher News
+//
+//  Created by Parth Nikam on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Phisher_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
