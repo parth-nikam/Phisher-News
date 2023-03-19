7b3b2800bda46f67b4070b18e27ea3feac45f575
parth-nikam <parthsnikam12co@gmail.com>
2023-03-19 12:20:49 +0530

    Used Observer design Pattern. Displaying points & Title

--- Phisher News/ContentView.swift
+++ Phisher News/ContentView.swift
@@ -8,15 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView{
-            List(posts){ post in
-                Text(post.title)
+            List(networkManager.posts){ post in
+                HStack {
+                    Text(String(post.points))
+                    Text(post.title)
+                }
             }
             .navigationBarTitle("Phisher News")
         }
-        
-        //        .padding()
+        .onAppear(){
+            self.networkManager.fetchData()
+        }
     }
 }
 
@@ -26,9 +33,9 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-let posts = [
-    
-    Post(id: "1", title: "Hello"),
-    Post(id: "2", title: "Bonjour"),
-    Post(id: "3", title: "Hola")
-]
+//let posts = [
+//
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Hola")
+//]
